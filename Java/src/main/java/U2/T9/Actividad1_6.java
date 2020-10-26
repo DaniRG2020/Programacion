e829ff095efb04b25a2fff06725ab521a184cbc0
DaniRG2020 <alum.drealg@iesalixar.org>
2020-10-26 18:41:13 +0100

    26/10/2020

---
+++ Java/src/main/java/U2/T9/Actividad1_6.java
@@ -0,0 +1,19 @@
+package U2.T9;
+
+import java.util.Scanner;
+
+public class Actividad1_6 {
+    public static void main(String[] args) {
+        //Escribir todos los múltiplos de 7 menores que 100.
+        Scanner sc = new Scanner(System.in);
+
+        for (int n = 1; n*7<100;n++){
+            int multiplo = n*7;
+                System.out.println(multiplo + " Es multiplo");
+
+
+            }
+
+        }
+    }
+
